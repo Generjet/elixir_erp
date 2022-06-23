a721494f64c02f65e9a2f51eae31ef791a22c7b8
Generjet <generjet@gmail.com>
2022-06-23 14:58:52 +0800

    bootstrap template applied

--- assets/js/app.js
+++ assets/js/app.js
@@ -25,7 +25,7 @@ import "phoenix_html"
 import {Socket} from "phoenix"
 import {LiveSocket} from "phoenix_live_view"
 import topbar from "../vendor/topbar"
-import ajax from "../vendor/ajax"
+// import ajax from "../vendor/ajax"
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
 let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}})
