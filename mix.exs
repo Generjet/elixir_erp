bed52bab9bc08f41152fdf761db5e2d69ac14917
Generjet <generjet@gmail.com>
2022-06-23 17:05:42 +0800

    oracle db connection option added

--- mix.exs
+++ mix.exs
@@ -49,7 +49,8 @@ defmodule Erp.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:jamdb_oracle, "~> 0.4.4"} # Add this line to connect with Oracle DB
     ]
   end
 
